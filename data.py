ecacd5c4472bddb638b0d54dc41be6ff41b0dab8
INGRID YAJAIRA SANTANA GARAY <inolga17@gmail.com>
2024-08-01 21:15:40 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,10 +1,21 @@
-user_body = {
-    "firstName": "Test",
-    "phone": "+1234567890",
-    "address": "123 Test St",
-
-}
-
 headers = {
     "Content-Type": "application/json"
+}
+user_body = {
+    "firstName": "Andrea",
+    "phone": "+11234567890",
+    "address": "123 Elm Street, Hilltop"
+}
+def get_kit_body(name):
+    return kit_bodies[name].copy()
+kit_bodies = {
+    "one_char": {"name": "a"},
+    "max_char": {"name": "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC"},
+    "empty_str": {"name":""},
+    "max_char_plus": {"name": "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD"},
+    "esp_char": {"name":"№%@"},
+    "space_char": {"name":"A Aaa"},
+    "numbers": {"name":"123"},
+    "no_param": {},
+    "dif_param": {"name":123}
 }
