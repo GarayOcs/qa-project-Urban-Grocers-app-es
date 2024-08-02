722a575a3c7b02cba7a11ec69d7fe0d8861c4450
INGRID YAJAIRA SANTANA GARAY <inolga17@gmail.com>
2024-08-01 21:16:03 -0600

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,19 +1,29 @@
 import configuration
-import data
 import requests
+import data
+def get_user_body(first_name):
+    # Copiar el diccionario con el cuerpo de la solicitud desde el archivo de datos
+    current_body = data.user_body.copy()
+    # Se cambia el valor del parámetro firstName
+    current_body["firstName"] = first_name
+    # Se devuelve un nuevo diccionario con el valor firstName requerido
+    return current_body
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
 
-def create_count(body):
-    return requests.post(
-        configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
-        json=body,
-        headers=data.headers
-    )
+response = post_new_user(data.user_body)
+def auth_token():
+    user = post_new_user(data.user_body)
+    user_json = user.json()
 
-def create_kit(kit_body, auth_token):
+    return user_json["authToken"]
+def post_new_client_kit(kit_body_key):
+    token = auth_token()
     headers = data.headers.copy()
-    headers['Authorization'] = f'Bearer {auth_token}'
-    return requests.post(
-        configuration.URL_SERVICE + configuration.KITS_PATH,
-        json=kit_body,
-        headers=headers
-    )
+    headers["Authorization"] = f'Bearer {token}'
+    kit_body = kit_body_key
+    return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
+                         json=kit_body,
+                         headers=headers)
