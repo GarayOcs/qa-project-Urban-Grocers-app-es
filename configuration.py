b30293429fb83e3f21666755b4b9dbebc8aa8449
INGRID YAJAIRA SANTANA GARAY <inolga17@gmail.com>
2024-08-01 21:14:36 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-21bf143b-9e0a-4374-acd9-c92d74a45469.containerhub.tripleten-services.com"
-CREATE_USER_PATH = "/api/v1/users"
-KITS_PATH = "/api/v1/kits"
+URL_SERVICE = "https://cnt-74992d12-af07-4806-a8d5-28378533a672.containerhub.tripleten-services.com"
+CREATE_USER_PATH = "/api/v1/users/"
+KITS_PATH = "/api/v1/kits/"
